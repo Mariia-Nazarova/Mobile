ddfee91f129d0685ed3bc55b5de2c0db2321cd41
Mariia Nazarova <Djungary@mail.ru>
2022-12-15 16:42:08 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         double balance = 100.20; //баланс моб тел, руб
-        double refill = 1300.51;//сумма пополнения, руб
+        double refill = 70;//сумма пополнения, руб
 
         int bonus = (int) refill / 100; //бонус за каждые 100 руб, руб
-        boolean promo = refill >= 1000; // пополнение на 1000 руб и выше
+        boolean promo = refill > 1000; // пополнение свыше 1000 руб
         double total = promo ? (balance + refill + bonus) : (balance + refill);
 
-        System.out.println("Начислено бонусов: " + bonus + " руб.");
+        if (promo) {
+            System.out.println("Начислено бонусов: " + bonus + " руб.");
+        } else {
+            System.out.println("Начислено бонусов: 0 руб.");
+        }
+
         System.out.println("Баланс: " + total + " руб.");
-
-
     }
 }
